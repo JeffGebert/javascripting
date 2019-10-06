16be0659c1e774ad53e6939a4488891ab9f7c629
Jeff Gebert <jeffgebert@hotmail.com>
2019-10-06 19:13:08 +0000

    finished javascripting

---
+++ function-arguments.js
@@ -0,0 +1,7 @@
+math = function (x,y,z ){
+  result1 = ((y*z) + x);
+  return result1;
+}
+
+console.log(math(53,61,67));
+
---
+++ looping-through-arrays.js
@@ -0,0 +1,8 @@
+pets = ['cat', 'dog', 'rat'];
+
+for (x=0; x < pets.length; x++) {
+  pets[x]=pets[x] + 's';
+}
+
+console.log(pets)
+
